d755ea6f7622ac991e120817e4d86ce16c525a28
Krakqn <139305417+Krakqn@users.noreply.github.com>
2024-08-01 19:36:01 +0000

    Fixed Bug #2. Updated the onClick handler to properly toggle the checked state and call the onChange function with the new state.

--- src/components/InputCheckbox/index.tsx
+++ src/components/InputCheckbox/index.tsx
@@ -1,9 +1,14 @@
-import classNames from "classnames"
-import { useRef } from "react"
-import { InputCheckboxComponent } from "./types"
+import classNames from "classnames";
+import { useRef } from "react";
+import { InputCheckboxComponent } from "./types";
 
-export const InputCheckbox: InputCheckboxComponent = ({ id, checked = false, disabled, onChange }) => {
-  const { current: inputId } = useRef(`RampInputCheckbox-${id}`)
+export const InputCheckbox: InputCheckboxComponent = ({
+  id,
+  checked = false,
+  disabled,
+  onChange,
+}) => {
+  const { current: inputId } = useRef(`RampInputCheckbox-${id}`);
 
   return (
     <div className="RampInputCheckbox--container" data-testid={inputId}>
@@ -12,6 +17,10 @@ export const InputCheckbox: InputCheckboxComponent = ({ id, checked = false, dis
           "RampInputCheckbox--label-checked": checked,
           "RampInputCheckbox--label-disabled": disabled,
         })}
+        onClick={() => onChange(!checked)}
+        // This fixes Bug #2
+        // It basically toggles "checked"
+        // properly
       />
       <input
         id={inputId}
@@ -22,5 +31,5 @@ export const InputCheckbox: InputCheckboxComponent = ({ id, checked = false, dis
         onChange={() => onChange(!checked)}
       />
     </div>
-  )
-}
+  );
+};
