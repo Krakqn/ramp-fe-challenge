f8690a9d1d74b02f76209a084bad366e92321a14
Krakqn <139305417+Krakqn@users.noreply.github.com>
2024-08-01 23:36:56 +0000

    Fixed Bug 7 by clearing cache for relevant endpoints after transaction approval changes.

--- src/components/Transactions/TransactionPane.tsx
+++ src/components/Transactions/TransactionPane.tsx
@@ -1,13 +1,15 @@
-import { useState } from "react"
-import { InputCheckbox } from "../InputCheckbox"
-import { TransactionPaneComponent } from "./types"
+import { useState, useEffect } from "react";
+import { InputCheckbox } from "../InputCheckbox";
+import { TransactionPaneComponent } from "./types";
+import { useCustomFetch } from "../../hooks/useCustomFetch";
 
 export const TransactionPane: TransactionPaneComponent = ({
   transaction,
   loading,
   setTransactionApproval: consumerSetTransactionApproval,
 }) => {
-  const [approved, setApproved] = useState(transaction.approved)
+  const [approved, setApproved] = useState(transaction.approved);
+  const { clearCacheByEndpoint } = useCustomFetch();
 
   return (
     <div className="RampPane">
@@ -15,7 +17,8 @@ export const TransactionPane: TransactionPaneComponent = ({
         <p className="RampText">{transaction.merchant} </p>
         <b>{moneyFormatter.format(transaction.amount)}</b>
         <p className="RampText--hushed RampText--s">
-          {transaction.employee.firstName} {transaction.employee.lastName} - {transaction.date}
+          {transaction.employee.firstName} {transaction.employee.lastName} -{" "}
+          {transaction.date}
         </p>
       </div>
       <InputCheckbox
@@ -26,16 +29,19 @@ export const TransactionPane: TransactionPaneComponent = ({
           await consumerSetTransactionApproval({
             transactionId: transaction.id,
             newValue,
-          })
-
-          setApproved(newValue)
+          });
+          await clearCacheByEndpoint([
+            "paginatedTransactions",
+            "transactionsByEmployee",
+          ]); // This fixes Bug 7 and ensures that the new value is used
+          setApproved(newValue);
         }}
       />
     </div>
-  )
-}
+  );
+};
 
 const moneyFormatter = new Intl.NumberFormat("en-US", {
   style: "currency",
   currency: "USD",
-})
+});
